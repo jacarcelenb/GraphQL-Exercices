0b496fbd0f18f80c14a3eacce9b7f58fd63aa078
jacarcelenb <carcelenjorge17@gmail.com>
2024-07-14 07:13:51 -0500

    Subida cambios

--- CHAPTER-05/03-MENUS-DINAMICOS/frontend/src/components/shared/welcome-page.js
+++ CHAPTER-05/03-MENUS-DINAMICOS/frontend/src/components/shared/welcome-page.js
@@ -4,10 +4,10 @@ import { Button } from "primereact/button";
 import { useQuery } from "@apollo/client";
 import { GET_USER_MENU } from "../../services/access-service";
 import { getRolUser, removeUserInfo } from "../../services/auth-service";
+import NavBar from "./navbar";
 const WelcomePage = () => {
   const [visible, setVisible] = useState(true);
   const [id, setId] = useState(getRolUser());
-
 
   const menulist = useQuery(GET_USER_MENU, {
     variables: {
@@ -17,35 +17,33 @@ const WelcomePage = () => {
 
   return (
     <div className=" flex justify-content-start">
-      <Sidebar visible={visible} onHide={() => setVisible(false)}
-        fullScreen>
-        <div style={{ display: "inline-flex" }}>
-          <h3 style={{ marginLeft: "5px" }}>Bienvenido/a</h3>
-          <img
-            src={require(".././../assets/images/pizza.jpg")}
-            className="img-login-main"
-            alt="..."
-            style={{ marginLeft: "5px" }}
+      <NavBar></NavBar>
+      <div style={{ display: "inline-flex" }}>
+        <h3 style={{ marginLeft: "5px" }}>Bienvenido/a</h3>
+        <br></br>
+        <img
+          src={require(".././../assets/images/pizza.jpg")}
+          alt="..."
+          style={{ marginLeft: "5px" }}
+        />
+        <br></br>
+        <h3 style={{ marginLeft: "5px" }}>
+          Tiene acceso a los siguientes menús:
+        </h3>
+      </div>
+
+      {menulist.data?.rolemenus[0].menus.map((item) => {
+        return (
+          <Button
+            key={item.mn_id}
+            label={item.mn_name}
+            icon={item.mn_icon}
+            text
+            className="side-bar-opt-button"
+            disabled
           />
-          <h3 style={{ marginLeft: "5px" }}>
-            Tiene acceso a los siguientes menús:
-          </h3>
-        </div>
-
-        {menulist.data?.rolemenus[0].menus.map((item) => {
-          return (
-            <Button
-              key={item.mn_id}
-              label={item.mn_name}
-              icon={item.mn_icon}
-              text
-              className="side-bar-opt-button"
-              disabled
-            />
-          );
-        })}
-      </Sidebar>
-
+        );
+      })}
     </div>
   );
 };
